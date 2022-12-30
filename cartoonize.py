87021897e87529d397a1ebeb0ba6c24f7b70e1ad
tuanPhantom <phanquangtuandev@gmail.com>
2022-12-31 02:11:53 +0700

    setup folders

--- cartoonize.py
+++ cartoonize.py
@@ -2,26 +2,26 @@ import os
 import cv2
 import numpy as np
 import tensorflow._api.v2.compat.v1 as tf
+
 tf.disable_eager_execution()
 import network
 import guided_filter
 from tqdm import tqdm
 
 
-
 def resize_crop(image):
     h, w, c = np.shape(image)
     if min(h, w) > 720:
         if h > w:
-            h, w = int(720*h/w), 720
+            h, w = int(720 * h / w), 720
         else:
-            h, w = 720, int(720*w/h)
+            h, w = 720, int(720 * w / h)
     image = cv2.resize(image, (w, h),
                        interpolation=cv2.INTER_AREA)
-    h, w = (h//8)*8, (w//8)*8
+    h, w = (h // 8) * 8, (w // 8) * 8
     image = image[:h, :w, :]
     return image
-    
+
 
 def cartoonize(load_folder, save_folder, model_path):
     input_photo = tf.placeholder(tf.float32, [1, None, None, 3])
@@ -31,7 +31,7 @@ def cartoonize(load_folder, save_folder, model_path):
     all_vars = tf.trainable_variables()
     gene_vars = [var for var in all_vars if 'generator' in var.name]
     saver = tf.train.Saver(var_list=gene_vars)
-    
+
     config = tf.ConfigProto()
     config.gpu_options.allow_growth = True
     sess = tf.Session(config=config)
@@ -45,17 +45,15 @@ def cartoonize(load_folder, save_folder, model_path):
             save_path = os.path.join(save_folder, name)
             image = cv2.imread(load_path)
             image = resize_crop(image)
-            batch_image = image.astype(np.float32)/127.5 - 1
+            batch_image = image.astype(np.float32) / 127.5 - 1
             batch_image = np.expand_dims(batch_image, axis=0)
             output = sess.run(final_out, feed_dict={input_photo: batch_image})
-            output = (np.squeeze(output)+1)*127.5
+            output = (np.squeeze(output) + 1) * 127.5
             output = np.clip(output, 0, 255).astype(np.uint8)
             cv2.imwrite(save_path, output)
         except:
             print('cartoonize {} failed'.format(load_path))
 
-
-    
 
 if __name__ == '__main__':
     model_path = 'saved_models'
@@ -64,6 +62,3 @@ if __name__ == '__main__':
     if not os.path.exists(save_folder):
         os.mkdir(save_folder)
     cartoonize(load_folder, save_folder, model_path)
-    
-
-    
